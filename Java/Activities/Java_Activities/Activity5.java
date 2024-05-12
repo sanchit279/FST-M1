e554d05319e837e06d741565e7a0ee59c92affb8
sanchit279 <36271545+sanchit279@users.noreply.github.com>
2024-05-12 13:27:09 +0530

    Add files via upload

---
+++ Java/Activities/Java_Activities/Activity5.java
@@ -0,0 +1,30 @@
+package Java_Activities;
+
+//Abstract class
+abstract class Book {
+    String title;
+    //Abstract method
+    abstract void setTitle(String s);
+
+    //Concrete method
+    String getTitle() {
+        return title;
+    }
+}
+
+class MyBook extends Book {
+    public void setTitle(String s) {
+        title = s;
+    }
+}
+
+public class Activity5 {
+
+    public static void main(String []args) {
+        String title = "Hover Car Racer";
+        Book newNovel = new MyBook();
+        newNovel.setTitle(title);
+
+        System.out.println("The title is: " + newNovel.getTitle());
+    }
+}
